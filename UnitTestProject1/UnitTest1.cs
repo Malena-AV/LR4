c704576c4f8b79048b4ca86f875c4702b903c957
maroze <grigorevaalla15@gmail.com>
2021-12-15 14:52:43 +0300

    сделано

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Mas.Number(new int[1] { 1 }), 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(Mas.Number(new int[] { }), 0);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(Mas.Number(new int[1] { 2 }), -1);
+        }
+    }
+}
