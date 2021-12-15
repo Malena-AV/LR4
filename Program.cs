946126660f208f13fcbe16102a4b6ea1ad04fdc3
maroze <grigorevaalla15@gmail.com>
2021-12-15 14:42:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[2] { 1,2 };
+            Console.WriteLine(Mas.Number(mas));
+        }
+    }
+}
